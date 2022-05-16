410d0fb53a3ad83a11d68975871b13ccaf266af5
Yuriy Shuin <yuriy5sh@gmail.com>
2022-05-16 21:11:44 +0300

    bug fix

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
   has_and_belongs_to_many :authorss
   belongs_to :binding_type
+
+  validates :title, presence: true, length: { minimum: 2 }
+  validates :pages, presence: true
+  validates :stoc_balance, presence: true
 end
