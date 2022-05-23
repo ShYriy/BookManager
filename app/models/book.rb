d745cff8f7c0020605596b760775d06dd0ca0f64
Yuriy Shuin <yuriy5sh@gmail.com>
2022-05-23 11:30:26 +0300

    add enum in book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,11 @@
 class Book < ApplicationRecord
-  has_and_belongs_to_many :authorss
+  has_and_belongs_to_many :authors
   belongs_to :binding_type
+
+  enum cover: {
+    hard: 1,
+    soft: 2
+  }
 
   validates :title, presence: true, length: { minimum: 2 }
   validates :pages_count, presence: true
