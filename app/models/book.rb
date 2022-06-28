4045f9e0fc4d014d91a4d7503d1271e300723cf2
Yuriy Shuin <yuriy5sh@gmail.com>
2022-06-28 16:57:18 +0300

    validates from item to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,8 @@ class Book < Item
     hard: 1,
     soft: 2
   }
+
+  validates :title, presence: true, length: { minimum: 2 }
+  validates :pages_count, presence: true
+  validates :stock_balance, presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,4 @@
 class Item < ApplicationRecord
   has_and_belongs_to_many :authors
   belongs_to :user
-
-  validates :title, presence: true, length: { minimum: 2 }
-  validates :pages_count, presence: true
-  validates :stock_balance, presence: true
 end
