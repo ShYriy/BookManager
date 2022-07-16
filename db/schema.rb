c7597120983e803480738fc4329d59602f9b2699
Yuriy Shuin <yuriy5sh@gmail.com>
2022-07-16 17:37:54 +0300

    migrate to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_06_27_133420) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -18,8 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_27_133420) do
   end
 
   create_table "authors_items", id: false, force: :cascade do |t|
-    t.integer "item_id"
-    t.integer "author_id"
+    t.bigint "item_id"
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_authors_items_on_author_id"
     t.index ["item_id"], name: "index_authors_items_on_item_id"
   end
@@ -31,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_27_133420) do
     t.integer "pages_count"
     t.integer "stock_balance"
     t.date "print_date"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "type"
     t.integer "cover"
     t.index ["user_id"], name: "index_items_on_user_id"
